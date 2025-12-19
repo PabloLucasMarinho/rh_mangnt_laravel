2e50dd657f3e0a52e57b6274adc48c6281f54a21
Pablo Marinho <pablolucas-1@outlook.com>
2025-12-19 18:10:57 -0300

    003

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+  // each department can be filled with multiple users
+  public function users()
+  {
+    return $this->belongsToMany(User::class);
+  }
 }
